760e189a0a146fe0178fa5d6407564b3204a9c03
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-06-20 19:37:33 +0530

    creating navbar

--- client/src/Component/Navbar/navbar.jsx
+++ client/src/Component/Navbar/navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import { useSelector, useDispatch} from 'react-redux'
+import bars from '../../Assets/bars-solid.svg'
+import logo from '../../Assets/logo.png'
+import search from '../../Assets/search-solid.svg'
 
 function navbar() {
+    var User = null;
   return (
-    <div>navbar</div>
+    <nav className="main-nav">
+        <div className="navbar">
+            <button className="slide-in-icon">
+                <img src={bars} alt="bars" width='15' />
+            </button>
+            <div className="navbar-1">
+                <Link to='/' className='nav-item nav-logo'>
+                    <img src={logo} alt="logo" />
+                </Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>About</Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>Products</Link>
+                <Link to='/' className='nav-item nav-btn res-nav'>For Teams</Link>
+                <form action="">
+                    <input type="text" placeholder="Search..." />
+                    <img src={search} alt="search" width='18' className='search-icon' />
+                </form>
+            </div>
+        </div>
+    </nav>
   )
 }
 
