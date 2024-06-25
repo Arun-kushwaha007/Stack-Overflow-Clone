1a11a33601985e5071ab3053ccb9ef5633c28fd4
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-06-25 20:14:35 +0530

    question page done

--- client/src/pages/Question/Displayanswer.jsx
+++ client/src/pages/Question/Displayanswer.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import Avatar from '../../Component/Avatar/Avatar';
 
-const Displayanswer = () => {
+
+const Displayanswer = ({question, handleshare}) => {
+  const user = null;
+  const handledelete=(answerid, noofanswer)=>{
+
+  }
   return (
-    <div>Displayanswer</div>
+    <div>
+      {
+        question.answer.map((ans)=>(
+          <div className="display-ans" key={ans._id}>
+            <p>{ans.answerbody}</p>
+            <div className="question-actions-user">
+              <div>
+                <button type='button' onClick={handleshare}> Share</button>
+                {user?.result?._id ===ans?.userid && (
+                  <button type='button' onClick={()=>handledelete(ans._id, question.noofanswer)} >Delete</button>
+                )}
+              </div>
+              <p>answered {moment(ans.answeredon).fromNow()}</p>
+              <Link to={`Users/${ans.userid}`} className='user-link' style={{color:"#0086d8"}}>
+                        <Avatar backgroundColor="orange" px="8px" py="5px" borderRadius="4px">
+                          {ans.useransered.charAt(0).toUpperCase()}
+                        </Avatar>
+                        <div>{ans.useransered}</div>
+                        </Link>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
