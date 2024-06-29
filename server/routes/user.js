0eb8987b0c6e3e0532e8f399958b7354ee673f8d
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-06-29 18:35:57 +0530

    users server

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,16 @@
-// import express from "express"
-const express = require('express')
+import express from "express"
+// const express = require('express')
 import { login,signup } from "../controller/auth.js"
-
+import { getallusers,updatedprofile } from "../controller/user.js";
+import auth from "../middleware/auth.js";
 const router =express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
 
+router.get("/getalluser",getallusers)
 
+router.patch("/update/:id",auth,updatedprofile)
 
 
 
