bad1d9523c6e7251d1617fc53b721c2b4478bfae
Arun Kushwaha <121769639+Arun-kushwaha007@users.noreply.github.com>
2024-07-01 22:27:03 +0530

    user auth.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,19 +2,17 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: "http://localhost:5000",
-    
 });
 
-API.interceptors.request.use((req)=>{
-    if(localStorage.getItem("Profile")){
-        req.headers.Authorization = `Bearer ${
-        JSON.parse(localStorage.getItem("Profile")).token}`
-    };
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem("Profile")) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("Profile")).token}`;
+    }
     return req;
-})
+});
 
-export const signup = (authdata)=> API.post("/user/signup",authdata);
-export const login = (authdata)=> API.post("/user/login",authdata);
+export const signup = (authdata) => API.post("/user/signup", authdata);
+export const login = (authdata) => API.post("/user/login", authdata);
 
-export const getallusers=()=> API.get("/user/getallusers");
-export const updateprofile = (id, updatedata)=> API.patch(`user/update/${id}`, updatedata)
+export const getallusers = () => API.get("/user/getallusers");
+export const updateprofile = (id, updatedata) => API.patch(`/user/update/${id}`, updatedata);
